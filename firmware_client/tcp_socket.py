178433d75009693bbc01ba871589c5782a79596c
Sana Max <mcsimoffa@yahoo.com>
2021-04-26 17:54:57 +0300

    socket in two threads

---
+++ firmware_client/tcp_socket.py
@@ -0,0 +1,23 @@
+import socket
+import threading
+
+def send_task():
+    while True:
+        strToSend = input("Строка для отправки:\r\n")
+        conn.send(strToSend.encode()) 
+            
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
+
+print ('connected:', addr)
+thread = threading.Thread(target=send_task)
+thread.start()
+
+while True:
+    data = conn.recv(1024)
+    if (len(data)>0):
+        print(data)    
+ 

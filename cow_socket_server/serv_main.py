4554b0690e24cc778a3896ebad70ad6af45e0227
Sana <mcsimoffa@yahoo.com>
2021-05-19 21:34:17 +0300

    server with select.select

--- cow_socket_server/serv_main.py
+++ cow_socket_server/serv_main.py
@@ -1,27 +1,52 @@
-import socket
-import threading
+import select, socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setblocking(0)
+server.bind(('', 9090))
+server.listen(5)
+inputs = [server]
+outputs = []
+message_queues = {}
 
-def send_task():
-    while True:
-        strToSend = input("String to send:\r\n")
-        conn.send(strToSend.encode()) 
+while inputs:
+    readable, writable, exceptional = select.select(inputs, outputs, inputs)
+    for s in readable:
+        if s is server:
+            connection, client_address = s.accept()
+            connection.setblocking(0)
+            inputs.append(connection)
+            print('\nsuccess client connected')
+        else:
+            data = s.recv(1024)
+            if data:
+                if message_queues.get(s, None):
+                    message_queues[s].append(data)
+                else:
+                    message_queues[s]=[data]
+                         
+                print('data receive_ ',data)
+                if s not in outputs:
+                    outputs.append(s)
+            else:
+                print('0 bytes receved')
+                if s in outputs:
+                    outputs.remove(s)
+                inputs.remove(s)
+                s.close()
+                del message_queues[s]
 
-def next_wait_task():
-    while True:
-        strToSend = input("String to send:\r\n")
-        conn.send(strToSend.encode())             
+    for s in writable:
+        print('writable',s)
+        next_msg = message_queues.get(s, None)
+        if len(next_msg):
+            temp=next_msg.pop(0).decode('utf-8').upper()
+            s.send(temp.encode())
+        else:
+            outputs.remove(s)
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
-
-print ('connected:', addr)
-thread = threading.Thread(target=send_task)
-thread.start()
-
-while True:
-    data = conn.recv(1024)
-    if (len(data)>0):
-        print(data)    
- 
+    for s in exceptional:
+        print('dissapear client')
+        inputs.remove(s)
+        if s in outputs:
+            outputs.remove(s)
+        s.close()
+        del message_queues[s]
